e6be5044f17e52adbece05728563340d27d2ce6f
vip933 <mdleypunskiy@edu.hse.ru>
2021-11-10 21:32:16 +0300

    Added article model.

--- mdleypunskiyPW5/Views/ArticleScreen/ArticleScreenModels.swift
+++ mdleypunskiyPW5/Views/ArticleScreen/ArticleScreenModels.swift
@@ -5,4 +5,42 @@
 //  Created by Maksim on 10.11.2021.
 //
 
-import Foundation
+import UIKit
+
+enum ArticleScreenModels {
+    enum Article {
+        struct Requast {}
+        struct Response {}
+        
+        struct ViewModel {}
+        
+        struct ArticlePage: Decodable {
+            var news: [ArticleModel]?
+            var requestId: String?
+            
+            mutating func passTheRequestId() {
+                for i in 0..<(news?.count ?? 0) {
+                    news?[i].requestId = requestId
+                }
+            }
+        }
+        
+        struct ArticleModel: Decodable {
+            var newsId: Int?
+            var titile: String?
+            var announce: String?
+            var img: ImageContainer?
+            var requestId: String?
+            
+            var articleUrl: URL? {
+                let requestId = requestId ?? ""
+                let newsId = newsId ?? 0
+                return URL(string: "https://news.myseldon.com/ru/news/index/\(newsId)?requestId=\(requestId)")
+            }
+        }
+        
+        struct ImageContainer: Decodable {
+            var url: URL?
+        }
+    }
+}
