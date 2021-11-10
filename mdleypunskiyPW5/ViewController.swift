bdd3804b29f51b5a1c6403080a546a3df20349aa
vip933 <mdleypunskiy@edu.hse.ru>
2021-11-10 19:51:51 +0300

    Initial Commit

---
+++ mdleypunskiyPW5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mdleypunskiyPW5
+//
+//  Created by Maksim on 10.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
